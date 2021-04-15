d6f73725eb9e2a10c9a9aba6d6bc173fc8827b6c
mastermind-computing <gokhan.danaci@togotechs.com>
2021-04-15 12:54:03 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
-	"time"
-	"unsafe"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
 func main() {
-	randFile := RandString(12)
+	randFile := SecureRandomAlphaString(12)
 	file, _ := os.Create("result" + randFile + ".txt")
-	for i := 0; i < 10000000; i++ {
-		tryBlock := RandString(64)
+	for i := 0; i < 100000000; i++ {
+		tryBlock := SecureRandomAlphaString(64)
 		h := sha256.New()
 		h.Write([]byte(tryBlock))
 		encodedString := fmt.Sprintf("%x", h.Sum(nil))
@@ -22,7 +28,7 @@ func main() {
 		if i%100000 == 0 {
 			fmt.Println(i)
 		}
-		if zeros > 2 {
+		if zeros > 10 {
 			fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 			fmt.Println(tryBlock)
 			fmt.Println("------------------------------------------------------------------------")
@@ -35,30 +41,57 @@ func main() {
 
 }
 
-//RandString get random string with high performance
-func RandString(n int) string {
-	var src = rand.NewSource(time.Now().UnixNano())
-	const letterBytes = "abcdef0123456789"
-	const (
-		letterIdxBits = 6                    // 6 bits to represent a letter index
-		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-	)
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
+// //RandString get random string with high performance
+// func RandString(n int) string {
+// 	var src = rand.NewSource(time.Now().UnixNano())
+// 	const letterBytes = "abcdef0123456789"
+// 	const (
+// 		letterIdxBits = 6                    // 6 bits to represent a letter index
+// 		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+// 		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+// 	)
+// 	b := make([]byte, n)
+// 	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+// 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+// 		if remain == 0 {
+// 			cache, remain = src.Int63(), letterIdxMax
+// 		}
+// 		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+// 			b[i] = letterBytes[idx]
+// 			i--
+// 		}
+// 		cache >>= letterIdxBits
+// 		remain--
+// 	}
+
+// 	return *(*string)(unsafe.Pointer(&b))
+// }
+
+func SecureRandomAlphaString(length int) string {
+
+	result := make([]byte, length)
+	bufferSize := int(float64(length) * 1.3)
+	for i, j, randomBytes := 0, 0, []byte{}; i < length; j++ {
+		if j%bufferSize == 0 {
+			randomBytes = SecureRandomBytes(bufferSize)
 		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
+		if idx := int(randomBytes[j%length] & letterIdxMask); idx < len(letterBytes) {
+			result[i] = letterBytes[idx]
+			i++
 		}
-		cache >>= letterIdxBits
-		remain--
 	}
 
-	return *(*string)(unsafe.Pointer(&b))
+	return string(result)
+}
+
+// SecureRandomBytes returns the requested number of bytes using crypto/rand
+func SecureRandomBytes(length int) []byte {
+	var randomBytes = make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Fatal("Unable to generate random bytes")
+	}
+	return randomBytes
 }
 
 func findStartingZeros(block string) int {
